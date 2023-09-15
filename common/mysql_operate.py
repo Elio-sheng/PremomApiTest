797cae74996f667e74e010e7c9af5371d08d13ac
Jessica <jessica.wei@easyhealthcarecorp.com>
2023-09-15 18:23:53 +0800

    调试MySQL方法并添加备注

--- common/mysql_operate.py
+++ common/mysql_operate.py
@@ -1,3 +1,5 @@
+# -*- coding: gbk -*-
+
 import pymysql
 import os
 from common.read_data import data
@@ -5,7 +7,7 @@ from common.logger import logger
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 data_file_path = os.path.join(BASE_PATH, "config", "setting.ini")
-data = data.load_ini(data_file_path)["mysql"]
+data = data.load_ini(data_file_path)["mysql"]     #获取config/setting.ini 文件里的MySQL配置数据
 
 DB_CONF = {
     "host": data["MYSQL_HOST"],
@@ -58,9 +60,9 @@ class MysqlDb:
         Returns:
             The query results as a list of dictionaries.
         """
-        self.conn.ping(reconnect=True)
-        self.cur.execute(sql)
-        data = self.cur.fetchall()
+        self.conn.ping(reconnect=True)    #确保数据库连接仍然有效。通过调用 ping 方法并传入 reconnect=True，它将会尝试重新连接数据库，从而保持连接的活跃状态。
+        self.cur.execute(sql)     #执行了传入的 SQL 查询语句。self.cur 是一个执行数据库操作的游标对象，通过调用 execute 方法并传入 sql 参数，实现执行查询的功能
+        data = self.cur.fetchall()   #获取查询结果集中的所有数据。self.cur.fetchall() 方法返回所有的查询结果，存储在名为 data 的变量中。
         return data
 
     def execute_db(self, sql):
@@ -80,3 +82,7 @@ class MysqlDb:
 
 
 db = MysqlDb(DB_CONF)
+
+if __name__ == '__main__':
+
+    db.select_db("select user_id,email from ezhome.all_login where email like 'test5_%';")
