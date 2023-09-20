aa5246a1f8e3d59d2cd9762a653f73d3c9e5dcb6
cindy <cindy.zhang@easyhealthcarecorp.com>
2023-09-20 09:51:59 +0800

    新增会员接口

--- testcases/user_service_test/test_userservice.py
+++ testcases/user_service_test/test_userservice.py
@@ -38,3 +38,15 @@ class TestLogin():
         # Print a message to indicate the end of the test case
         logger.info("*************** 结束执行用例 ***************")
 
+class TestMember():
+    def test_userservice_ismember(self, title,except_result, expect_code, expect_msg):
+        # Print a message to indicate the start of the test case
+        logger.info("*************** 开始执行用例 ***************")
+        # Call the webUserLogin function with the provided parameters
+        result = TestMember.ismember(title, except_result, expect_code,expect_msg)
+
+        # Log the expected result, code, and message
+
+        logger.info(result)
+        # Print a message to indicate the end of the test case
+        logger.info("*************** 结束执行用例 ***************")
