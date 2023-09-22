e2c9d0259c014537a9c0673d90c712e8da37e67e
Jessica <jessica.wei@easyhealthcarecorp.com>
2023-09-22 12:05:26 +0800

    新增获取Homepage页面信息接口

--- testcases/misc_service_test/conftest.py
+++ testcases/misc_service_test/conftest.py
@@ -7,9 +7,10 @@ from common.logger import logger
 from common.read_data import yaml
 from api.user import UserService
 
-BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))).split("testcases")[1]
-logger.info(BASE_PATH)
-userTestdata = None
+BASE_PATH = os.path.dirname(os.path.abspath(__file__)).split("testcases")[0]
+# logger.info(BASE_PATH)
+miscTestdata = None
+
 
 def get_data(yaml_file_name):
     data_file_path = os.path.join(BASE_PATH, "data", yaml_file_name)
@@ -18,13 +19,13 @@ def get_data(yaml_file_name):
         with open(data_file_path, 'r', encoding='utf-8') as file:
             yaml_data = yaml.safe_load(file)
     except FileNotFoundError:
-        logger.info("读不到 ==>> {}".format(data_file_path))
         pytest.skip(f"File not found: {data_file_path}")
     except Exception as ex:
+        logger.info(yaml_file_name)
+        logger.info(ex)
         pytest.skip(f"Error loading YAML file: {ex}")
     else:
         return yaml_data
-
 
 
 def pytest_generate_tests(metafunc):
@@ -38,32 +39,33 @@ def pytest_generate_tests(metafunc):
               - [valA1, valA2]          # Test cases
               - [valA2, valB2]
     """
-    global userTestdata
-    if not userTestdata:
+    global miscTestdata
+    if not miscTestdata:
         # Fetch testdata from YAML file
-        userTestdata = get_data("userservice_test_data.yml")
+        miscTestdata = get_data("miscservice_test_data.yml")
 
     classname = metafunc.cls.__name__
     funcname = metafunc.function.__name__
-    funcdata = userTestdata.get(classname)[funcname]
+    funcdata = miscTestdata.get(classname)[funcname]
     if funcdata:
         parameters = funcdata['parameters']
         # Convert argument lists to tuples
         values = [tuple(v) if isinstance(v, list) else v for v in funcdata['values']]
-        # logger.info(f"Parameters: {parameters}")
-        # logger.info(f"Values: {values}")
+        logger.info(f"Parameters: {parameters}")
+        logger.info(f"Values: {values}")
         metafunc.parametrize(parameters, values)
 
+
 @pytest.fixture(scope="session", autouse=True)
-def userToken():
+def misc_token():
     json_data = {
-      "anonymousId": "",
-      "bindAnonymous": "true",
-      "email": "test771@premom.com",
-      "password": "123456",
-      "phoneID": "decbb1ef-e41c-4cbd-9265-902415f00504",
-      "platform": "iPhone XR 13.3",
-      "timeZone": "+0800"
+        "anonymousId": "",
+        "bindAnonymous": "true",
+        "email": "test773@premom.com",
+        "password": "123456",
+        "phoneID": "decbb1ef-e41c-4cbd-9265-902415f00504",
+        "platform": "iPhone XR 13.3",
+        "timeZone": "+0800"
     }
     header = {
         "Content-Type": "application/json"
@@ -71,4 +73,5 @@ def userToken():
     userservice = UserService()
     res = userservice.webUserLogin(json=json_data, headers=header)
     token = res.headers.get("authToken")
+    logger.info(token)
     return token
---
+++ testcases/misc_service_test/test_miscservice.py
@@ -0,0 +1,16 @@
+"""
+-*- coding: utf-8 -*-
+"""
+import pytest
+from common.logger import logger
+from operation.miscserevice import Misc
+from api.user import UserService
+
+misc_request = Misc()
+class TestMisc():
+    def test_miscservice_homepage(self, title, pageName, except_result, expect_code, expect_msg,misc_token):
+        logger.info("*************** 开始执行用例 ***************")
+        result = misc_request.homepage( title, pageName, except_result, expect_code, expect_msg, misc_token)
+        logger.info(result)
+        # Print a message to indicate the end of the test case
+        logger.info("*************** 结束执行用例 ***************")
