7f1caf94de16b8322e3ab3e9df599c4cb7c5aabf
Jessica <jessica.wei@easyhealthcarecorp.com>
2023-09-26 11:06:52 +0800

    update guarantee

--- testcases/user_service_test/conftest.py
+++ testcases/user_service_test/conftest.py
@@ -59,7 +59,7 @@ def userToken():
     json_data = {
       "anonymousId": "",
       "bindAnonymous": "true",
-      "email": "test771@premom.com",
+      "email": "test777@premom.com",
       "password": "123456",
       "phoneID": "decbb1ef-e41c-4cbd-9265-902415f00504",
       "platform": "iPhone XR 13.3",
--- testcases/user_service_test/test_userservice.py
+++ testcases/user_service_test/test_userservice.py
@@ -11,12 +11,9 @@ class TestLogin():
     def test_userservice_login(self, title, anonymousId, bindAnonymous, email, password, phoneID, platform, timeZone, except_result, expect_code, expect_msg):
         # Print a message to indicate the start of the test case
         logger.info("*************** 开始执行用例 ***************")
-
         # Call the webUserLogin function with the provided parameters
         result = userInstance.webUserLogin(title, anonymousId, bindAnonymous, email, password, phoneID, platform, timeZone, except_result, expect_code,expect_msg)
-
         # Log the expected result, code, and message
-
         logger.info(result)
         # Print a message to indicate the end of the test case
         logger.info("*************** 结束执行用例 ***************")
@@ -35,7 +32,7 @@ class TestLogin():
     #     # Print a message to indicate the start of the test case
     #     logger.info("*************** 开始执行用例 ***************")
     #     # Call the webUserDelete function with the provided parameters
-    #     result = User().myProfile(title, except_result, expect_code,expect_msg,userToken)
+    #     result = userInstance.myProfile(title, except_result, expect_code,expect_msg,userToken)
     #     logger.info(result)
     #     # Print a message to indicate the end of the test case
     #     logger.info("*************** 结束执行用例 ***************")
@@ -45,15 +42,15 @@ class TestLogin():
     #     logger.info("*************** 开始执行用例 ***************")
     #     logger.info(userToken)
     #     # Call the webUserDelete function with the provided parameters
-    #     result = User().userDelete(title, reasonType, appsflyerId, except_result, expect_code, expect_msg, userToken)
+    #     result = userInstance.userDelete(title, reasonType, appsflyerId, except_result, expect_code, expect_msg, userToken)
     #     logger.info(result)
     #     # Print a message to indicate the end of the test case
     #     logger.info("*************** 结束执行用例 ***************")
 
-    def test_userservice_guarantee(self, title, addFreeTestsActivity, params, except_result, expect_code, expect_msg):
+    def test_userservice_guarantee(self, title, addFreeTestsActivity, params, except_result, expect_code, expect_msg, userToken):
         # Print a message to indicate the start of the test case
         logger.info("*************** 开始执行用例 ***************")
-        result = userInstance.userGuarantee(title, addFreeTestsActivity, params, except_result, expect_code, expect_msg)
+        result = userInstance.userGuarantee(title, addFreeTestsActivity, params, except_result, expect_code, expect_msg, userToken)
         # Log the expected result, code, and message
         logger.info(result)
         # Print a message to indicate the end of the test case
@@ -68,9 +65,7 @@ class TestLogin():
 #         logger.info("*************** 开始执行用例 ***************")
 #         # Call the webUserLogin function with the provided parameters
 #         result = Member().ismember(title, except_result, expect_code,expect_msg,global_token)
-#
 #         # Log the expected result, code, and message
-#
 #         logger.info(result)
 #         # Print a message to indicate the end of the test case
 #         logger.info("*************** 结束执行用例 ***************")
