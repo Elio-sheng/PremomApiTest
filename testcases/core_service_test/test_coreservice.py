90b84a2811df6c7c5318464276de7c984cb31364
Jessica <jessica.wei@easyhealthcarecorp.com>
2023-10-13 17:29:45 +0800

    修改冲突

--- testcases/core_service_test/test_coreservice.py
+++ testcases/core_service_test/test_coreservice.py
@@ -52,36 +52,27 @@ class TestReport():
         logger.info("*************** 结束执行用例 ***************")
 
 
-#
-# class TestInsemination():
-#     def test_coreservice_InseminationAddOrUpdate(self, title, id, frozenSperm, noInsemination, noneOfTheAbove, triggerShot, type, expect_code, expect_msg, core_token, get_current_date):
-#         logger.info("*************** 开始执行用例 ***************")
-#         result = Insemination().inseminationAddOrUpdate(title, id, frozenSperm, noInsemination, noneOfTheAbove, triggerShot, type, expect_code, expect_msg, core_token, get_current_date)
-#         logger.info(result)
-#         logger.info("*************** 结束执行用例 ***************")
-#
-#     def test_coreservice_inseminationDataGet(self, title, recordDate, expect_code, expect_msg, core_token):
-#         logger.info("*************** 开始执行用例 ***************")
-#         result = Insemination().inseminationDataGet(title, recordDate, expect_code, expect_msg, core_token)
-#         logger.info(result)
-#         logger.info("*************** 结束执行用例 ***************")
-#
-#     def test_coreservice_dailyRecordGetSetting(self, title, expect_code, expect_msg, core_token):
-#         logger.info("*************** 开始执行用例 ***************")
-#         result = Insemination().dailyRecordGetSetting(title,  expect_code, expect_msg, core_token)
-#         logger.info(result)
-#         logger.info("*************** 结束执行用例 ***************")
-#
-#
-#     def test_coreservice_inseminationGetTip(self, title, editCycle, editDate, expect_code, expect_msg, core_token):
-#         logger.info("*************** 开始执行用例 ***************")
-#         result = Insemination().inseminationGetTip(title, editCycle, editDate, expect_code, expect_msg, core_token)
-#         logger.info(result)
-#         logger.info("*************** 结束执行用例 ***************")
-#
-#
-#
-#
+
+class TestInsemination():
+    def test_coreservice_InseminationAddOrUpdate(self, title, id, frozenSperm, noInsemination, noneOfTheAbove, triggerShot, type, expect_code, expect_msg, core_token, get_current_date):
+        logger.info("*************** 开始执行用例 ***************")
+        result = Insemination().inseminationAddOrUpdate(title, id, frozenSperm, noInsemination, noneOfTheAbove, triggerShot, type, expect_code, expect_msg, core_token, get_current_date)
+        logger.info(result)
+        logger.info("*************** 结束执行用例 ***************")
+
+    def test_coreservice_inseminationDataGet(self, title, recordDate, expect_code, expect_msg, core_token):
+        logger.info("*************** 开始执行用例 ***************")
+        result = Insemination().inseminationDataGet(title, recordDate, expect_code, expect_msg, core_token)
+        logger.info(result)
+        logger.info("*************** 结束执行用例 ***************")
+
+    def test_coreservice_dailyRecordGetSetting(self, title, expect_code, expect_msg, core_token):
+        logger.info("*************** 开始执行用例 ***************")
+        result = Insemination().dailyRecordGetSetting(title,  expect_code, expect_msg, core_token)
+        logger.info(result)
+        logger.info("*************** 结束执行用例 ***************")
+
+
     def test_coreservice_inseminationGetTip(self, title, editCycle, editDate, expect_code, expect_msg, core_token):
         """获取提示文案"""
         logger.info("*************** 开始执行用例 ***************")
